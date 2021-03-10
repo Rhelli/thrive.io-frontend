365212115260c64642cad0c5b54a21ced8855643
roryhellier <rpry.hellier@googlemail.com>
2021-03-10 21:23:13 +0000

    Adds First Build Of The PropertyContainer:
     - Adds and required imports as well as components
     - Intgrates the redux propertyStore and passes mapStateToProps
     - Creates render, containing all components that will be rendered
     - Adds propTypes validation

--- src/pages/Property/PropertyContainer.js
+++ src/pages/Property/PropertyContainer.js
@@ -1,5 +1,27 @@
-const PropertyContainer = () => (
-  <h2>PropertyContainer</h2>
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import PropertyAboutComponent from './components/PropertyAboutComponent';
+import PropertyImageComponent from './components/PropertyImageComponent';
+import PropertyInfoComponent from './components/PropertyInfoComponent';
+
+const PropertyContainer = ({ propertyProfile }) => (
+  <div className={styles.propertyContainer}>
+    <PropertyImageComponent propertyProfile={propertyProfile} />
+    <PropertyAboutComponent propertyProfile={propertyProfile} />
+    <PropertyInfoComponent propertyProfile={propertyProfile} />
+  </div>
 );
 
-export default PropertyContainer;
+PropertyContainer.propTypes = {
+  propertyProfile: PropTypes.shape({
+    properties: PropTypes.objectOf([PropTypes.string, PropTypes.array, PropTypes.number]),
+    singleProperty: PropTypes.arrayOf([PropTypes.string, PropTypes.array, PropTypes.number]),
+  }).isRequired,
+};
+
+const mapStateToProps = state => ({
+  propertyProfile: state.propertyStore,
+});
+
+export default connect(mapStateToProps, null)(PropertyContainer);
--- src/pages/UserProfile/UserProfileContainer.js
+++ src/pages/UserProfile/UserProfileContainer.js
@@ -14,7 +14,6 @@ const UserProfileContainer = ({
   return (
     <div>
       {profileInfo.name}
-      <p>Motherfucker Jones</p>
     </div>
   );
 };
