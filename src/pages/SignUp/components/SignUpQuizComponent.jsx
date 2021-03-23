32cb7ec51fdb600b00bd5de6557d82457175ef56
roryhellier <rpry.hellier@googlemail.com>
2021-03-23 23:27:58 +0000

    Begins Modifying The SignUpQuizComponent To Add New User Fields:
     - Adds useState to imports and defines userType and setUserType states to be used for showing the advertising_type input fields

--- src/pages/SignUp/components/SignUpQuizComponent.jsx
+++ src/pages/SignUp/components/SignUpQuizComponent.jsx
@@ -1,53 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const SignUpQuizComponent = ({ createUserRequest }) => (
-  <div>
-    <form onSubmit={event => createUserRequest(event)}>
-      <div>
-        <h1>Sign Up</h1>
-        <p>Please complete this form to create a Thrive account</p>
-        <hr />
+const SignUpQuizComponent = ({ createUserRequest }) => {
+  const [userType, setUserType] = useState(null);
 
-        <label htmlFor="name">
-          Name
-          <input id="name" type="text" required />
-        </label>
-
-        <label htmlFor="email">
-          Email
-          <input id="email" type="text" required />
-        </label>
-
-        <label htmlFor="password">
-          Password
-          <input id="password" type="password" required />
-        </label>
-        <hr />
-
-        <h3>Are you looking or advertising for a place?</h3>
+  return (
+    <div>
+      <form onSubmit={event => createUserRequest(event)}>
         <div>
-          <span>
-            <label htmlFor="looking">
-              Looking
-              <input type="radio" id="looking" name="userType" value="Looking" />
-            </label>
-          </span>
-          <span>
-            <label htmlFor="advertising">
-              Advertising
-              <input type="radio" id="advertising" name="userType" value="Advertising" />
-            </label>
-          </span>
-        </div>
+          <h1>Sign Up</h1>
+          <p>Please complete this form to create a Thrive account</p>
+          <hr />
 
-        <div>
-          <button type="submit">Create Account</button>
+          <label htmlFor="name">
+            Name
+            <input id="name" type="text" required />
+          </label>
+
+          <label htmlFor="email">
+            Email
+            <input id="email" type="text" required />
+          </label>
+
+          <label htmlFor="dob">
+            Date Of Birth
+            <input id="dob" type="date" required />
+          </label>
+
+          <label htmlFor="password">
+            Password
+            <input id="password" type="password" required />
+          </label>
+          <hr />
+
+          <h3>Are you looking or advertising for a place?</h3>
+          <div>
+            <span>
+              <label htmlFor="looking">
+                Looking
+                <input type="radio" id="looking" name="userType" value="Looking" onChange={setUserType('looking')} />
+              </label>
+            </span>
+            <span>
+              <label htmlFor="advertising">
+                Advertising
+                <input type="radio" id="advertising" name="userType" value="Advertising" onChange={setUserType('advertising')} />
+              </label>
+            </span>
+          </div>
+
+          <div>
+            <button type="submit">Create Account</button>
+          </div>
         </div>
-      </div>
-    </form>
-  </div>
-);
+      </form>
+    </div>
+  );
+};
 
 SignUpQuizComponent.propTypes = {
   createUserRequest: PropTypes.func.isRequired,
