7fd1bdd682734a1a51e3e3ce88bf0b678ff3990f
Rhelli <rory.hellier@googlemail.com>
2021-04-08 23:14:56 +0100

    Reconfigures And Styles The PropertyFormComponent And The EditProperty Peripheral Components:
     - Modifies the PropertyFormComponent in common to apply styles
     - Modifies the EditProperty component to add styles

--- src/pages/EditProperty/components/EditPropertyNavbarComponent/EditPropertyNavbarComponent.jsx
+++ src/pages/EditProperty/components/EditPropertyNavbarComponent/EditPropertyNavbarComponent.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import tLogo from '../../../../assets/img/thrive-t-transparent.png';
 import styles from './EditPropertyNavbarComponent.module.scss';
 
 const EditPropertyNavbarComponent = ({ handleNavbarBackButton }) => (
   <header className={styles.editPropertyNavbarContainer}>
-    <button onClick={handleNavbarBackButton} type="button">
-      <FontAwesomeIcon icon={faChevronLeft} />
-    </button>
-    <h2>Edit Property</h2>
+    <span className={styles.titleSpan}>
+      <button onClick={handleNavbarBackButton} type="button">
+        <FontAwesomeIcon icon={faChevronLeft} />
+      </button>
+      <h1>Edit Property</h1>
+    </span>
+    <span className={styles.imageSpan}>
+      <img src={tLogo} alt="Thrive" />
+    </span>
   </header>
 );
 
