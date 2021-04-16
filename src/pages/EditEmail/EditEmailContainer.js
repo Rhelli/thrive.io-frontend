e06ed092a4807d2cea44f40623d4e5b449bfadac
Rhelli <rory.hellier@googlemail.com>
2021-04-16 21:01:15 +0100

    Adds Form Validation to And Modifies The Edit Email Form:
     - Adds useState to the EditEmailContainer and Component for tracking the current email string
     - Adds validation for the email string
     - Adds removal of the validation string when the user changes the field back:

--- src/pages/EditEmail/EditEmailContainer.js
+++ src/pages/EditEmail/EditEmailContainer.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import formValidator from '../../utils/FormUtils';
 import ProfileSettingsNavbar from '../../common/ProfileSettingsNavbar/ProfileSettingsNavbar';
 import { updateCurrentUserEmailApiRequest } from '../../api/profileSettingsApi';
 import EditEmailFormComponent from './components/EditEmailFormComponent/EditEmailFormComponent';
@@ -10,11 +11,17 @@ import styles from './EditEmailContainer.module.scss';
 const EditEmailContainer = ({ userProfile, updateCurrentUserEmailApiRequest }) => {
   const history = useHistory();
   const handleBackButtonClick = () => history.push('/myaccount/settings');
+  const [email, setEmail] = useState(userProfile.email);
+  const [emailError, setEmailError] = useState(null);
 
   const handleEmailUpdate = event => {
     event.preventDefault();
-    updateCurrentUserEmailApiRequest(event);
-    history.push('/myaccount');
+    if (!formValidator(email, 'email')) {
+      updateCurrentUserEmailApiRequest(event);
+      history.push('/myaccount');
+    } else {
+      setEmailError('Invalid email address. Please try again.');
+    }
   };
 
   return (
@@ -23,6 +30,9 @@ const EditEmailContainer = ({ userProfile, updateCurrentUserEmailApiRequest }) =
       <EditEmailFormComponent
         userProfile={userProfile}
         handleEmailUpdate={handleEmailUpdate}
+        emailError={emailError}
+        setEmail={setEmail}
+        setEmailError={setEmailError}
       />
     </div>
   );
