d6de0a09491abff6334e63e1d52af852d9bdfa9b
Rhelli <rory.hellier@googlemail.com>
2021-04-20 17:42:25 +0100

    Adds And Finishes Testing The PropertyFormComponent

--- src/common/PropertyFormComponent/__tests__/PropertyFormComponent.test.js
+++ src/common/PropertyFormComponent/__tests__/PropertyFormComponent.test.js
@@ -19,7 +19,7 @@ const singleProperty = {
   furnished: 'Furnished',
   genders: ['Male', 'Male', 'Male'],
   id: 2,
-  internet: 'Internet Included',
+  internet: 'Internet',
   maxAge: 29,
   minAge: 26,
   occupantCount: 3,
@@ -292,4 +292,189 @@ describe('The appearance and functionality of the PropertyForm components text i
     expect(minAgeInput.instance().value).toBe('26');
     expect(maxAgeInput.instance().value).toBe('29');
   });
+
+  it('Contains the text input field for the number of genders in the house', () => {
+    const container = mount(emptyPropertyFormComponent);
+    const genderInputs = container.find('#genderInput');
+    const maleInput = genderInputs.find('#male');
+    const femaleInput = genderInputs.find('#female');
+    const transInput = genderInputs.find('#trans');
+    const otherInput = genderInputs.find('#other');
+    expect(maleInput.instance().value).toBe('0');
+    expect(femaleInput.instance().value).toBe('0');
+    expect(transInput.instance().value).toBe('0');
+    expect(otherInput.instance().value).toBe('0');
+  });
+
+  it('Pre-populates the number of genders in the house if provided property information', () => {
+    const container = mount(propertyFormComponent);
+    const genderInputs = container.find('#genderInput');
+    const maleInput = genderInputs.find('#male');
+    const femaleInput = genderInputs.find('#female');
+    const transInput = genderInputs.find('#trans');
+    const otherInput = genderInputs.find('#other');
+    expect(maleInput.instance().value).toBe('3');
+    expect(femaleInput.instance().value).toBe('0');
+    expect(transInput.instance().value).toBe('0');
+    expect(otherInput.instance().value).toBe('0');
+  });
 });
+
+describe('The appearance and functionality of the PropertyForm components radio input fields', () => {
+  it('Displays the radio button fields for selection the bills option correctly', () => {
+    const container = mount(emptyPropertyFormComponent);
+    const billsRadioField = container.find('#billsInput');
+    expect(billsRadioField.childAt(0).name()).toBe('h3');
+    expect(billsRadioField.childAt(0).text()).toBe('Are Bills Included? *');
+    const includedField = billsRadioField.childAt(1);
+    const notIncludedField = billsRadioField.childAt(2);
+    expect(includedField.childAt(0).name()).toBe('input');
+    expect(includedField.childAt(1).name()).toBe('label');
+    expect(includedField.childAt(1).text()).toBe('Included');
+    expect(notIncludedField.childAt(0).name()).toBe('input');
+    expect(notIncludedField.childAt(1).name()).toBe('label');
+    expect(notIncludedField.childAt(1).text()).toBe('Not Included');
+  });
+
+  it('Auto-checks the radio buttons when property information is provided', () => {
+    const container = mount(propertyFormComponent);
+    const billsRadioField = container.find('#billsInput');
+    const includedField = billsRadioField.childAt(1);
+    const notIncludedField = billsRadioField.childAt(2);
+    expect(includedField.childAt(0).instance().checked).toBe(true);
+    expect(notIncludedField.childAt(0).instance().checked).toBe(false);
+  });
+
+  it('Displays the radio button fields for selection the furnished option correctly', () => {
+    const container = mount(emptyPropertyFormComponent);
+    const furnishedRadioField = container.find('#furnishedInput');
+    expect(furnishedRadioField.childAt(0).name()).toBe('h3');
+    expect(furnishedRadioField.childAt(0).text()).toBe('Is The Space For Rent Furnished? *');
+    const furnishedField = furnishedRadioField.childAt(1);
+    const nonFurnishedField = furnishedRadioField.childAt(2);
+    expect(furnishedField.childAt(0).name()).toBe('input');
+    expect(furnishedField.childAt(1).name()).toBe('label');
+    expect(furnishedField.childAt(1).text()).toBe('Furnished');
+    expect(nonFurnishedField.childAt(0).name()).toBe('input');
+    expect(nonFurnishedField.childAt(1).name()).toBe('label');
+    expect(nonFurnishedField.childAt(1).text()).toBe('Non-Furnished');
+  });
+
+  it('Auto-checks the radio buttons when property information is provided', () => {
+    const container = mount(propertyFormComponent);
+    const furnishedRadioField = container.find('#furnishedInput');
+    const furnishedField = furnishedRadioField.childAt(1);
+    const nonFurnishedField = furnishedRadioField.childAt(2);
+    expect(furnishedField.childAt(0).instance().checked).toBe(true);
+    expect(nonFurnishedField.childAt(0).instance().checked).toBe(false);
+  });
+
+  it('Displays the radio button fields for selection the parking option correctly', () => {
+    const container = mount(emptyPropertyFormComponent);
+    const parkingRadioField = container.find('#parkingInput');
+    expect(parkingRadioField.childAt(0).name()).toBe('h3');
+    expect(parkingRadioField.childAt(0).text()).toBe('Is There Parking? *');
+    const parkingField = parkingRadioField.childAt(1);
+    const nonParkingField = parkingRadioField.childAt(2);
+    expect(parkingField.childAt(0).name()).toBe('input');
+    expect(parkingField.childAt(1).name()).toBe('label');
+    expect(parkingField.childAt(1).text()).toBe('Parking');
+    expect(nonParkingField.childAt(0).name()).toBe('input');
+    expect(nonParkingField.childAt(1).name()).toBe('label');
+    expect(nonParkingField.childAt(1).text()).toBe('No Parking');
+  });
+
+  it('Auto-checks the parking radio buttons when property information is provided', () => {
+    const container = mount(propertyFormComponent);
+    const parkingRadioField = container.find('#parkingInput');
+    const parkingField = parkingRadioField.childAt(1);
+    const nonParkingField = parkingRadioField.childAt(2);
+    expect(parkingField.childAt(0).instance().checked).toBe(false);
+    expect(nonParkingField.childAt(0).instance().checked).toBe(true);
+  });
+
+  it('Displays the radio button fields for if there is disabled access correctly', () => {
+    const container = mount(emptyPropertyFormComponent);
+    const disabledRadioField = container.find('#disabledInput');
+    expect(disabledRadioField.childAt(0).name()).toBe('h3');
+    expect(disabledRadioField.childAt(0).text()).toBe('Is There Disabled Access?');
+    const disabledField = disabledRadioField.childAt(1);
+    const nonDisabledField = disabledRadioField.childAt(2);
+    expect(disabledField.childAt(0).name()).toBe('input');
+    expect(disabledField.childAt(1).name()).toBe('label');
+    expect(disabledField.childAt(1).text()).toBe('Disabled Access');
+    expect(nonDisabledField.childAt(0).name()).toBe('input');
+    expect(nonDisabledField.childAt(1).name()).toBe('label');
+    expect(nonDisabledField.childAt(1).text()).toBe('No Disabled Access');
+  });
+
+  it('Auto-checks the disabled access radio buttons when property information is provided', () => {
+    const container = mount(propertyFormComponent);
+    const disabledRadioField = container.find('#disabledInput');
+    const disabledField = disabledRadioField.childAt(1);
+    const nonDisabledField = disabledRadioField.childAt(2);
+    expect(disabledField.childAt(0).instance().checked).toBe(false);
+    expect(nonDisabledField.childAt(0).instance().checked).toBe(true);
+  });
+
+  it('Displays the radio button fields for the internet option correctly', () => {
+    const container = mount(emptyPropertyFormComponent);
+    const internetRadioField = container.find('#internetInput');
+    expect(internetRadioField.childAt(0).name()).toBe('h3');
+    expect(internetRadioField.childAt(0).text()).toBe('Is Internet Provided?');
+    const internetField = internetRadioField.childAt(1);
+    const noInternetField = internetRadioField.childAt(2);
+    expect(internetField.childAt(0).name()).toBe('input');
+    expect(internetField.childAt(1).name()).toBe('label');
+    expect(internetField.childAt(1).text()).toBe('Internet Provided');
+    expect(noInternetField.childAt(0).name()).toBe('input');
+    expect(noInternetField.childAt(1).name()).toBe('label');
+    expect(noInternetField.childAt(1).text()).toBe('No Internet Provided');
+  });
+
+  it('Auto-checks the internet radio buttons when property information is provided', () => {
+    const container = mount(propertyFormComponent);
+    const internetRadioField = container.find('#internetInput');
+    const internetField = internetRadioField.childAt(1);
+    const noInternetField = internetRadioField.childAt(2);
+    expect(internetField.childAt(0).instance().checked).toBe(true);
+    expect(noInternetField.childAt(0).instance().checked).toBe(false);
+  });
+
+  it('Displays the radio button fields for the smoking options correctly', () => {
+    const container = mount(emptyPropertyFormComponent);
+    const smokingRadioField = container.find('#smokingInput');
+    expect(smokingRadioField.childAt(0).name()).toBe('h3');
+    expect(smokingRadioField.childAt(0).text()).toBe('Is Smoking Allowed?');
+    const smokingField = smokingRadioField.childAt(1);
+    const nonSmokingField = smokingRadioField.childAt(2);
+    const anySmokingField = smokingRadioField.childAt(3);
+
+    expect(smokingField.childAt(1).name()).toBe('label');
+    expect(smokingField.childAt(1).text()).toBe('Smoking');
+    expect(smokingField.childAt(0).name()).toBe('input');
+    expect(smokingField.childAt(0).instance().checked).toBe(false);
+
+    expect(nonSmokingField.childAt(1).name()).toBe('label');
+    expect(nonSmokingField.childAt(1).text()).toBe('Non-Smoking');
+    expect(nonSmokingField.childAt(0).name()).toBe('input');
+    expect(nonSmokingField.childAt(0).instance().checked).toBe(false);
+
+    expect(anySmokingField.childAt(1).name()).toBe('label');
+    expect(anySmokingField.childAt(1).text()).toBe('Any');
+    expect(anySmokingField.childAt(0).name()).toBe('input');
+    expect(anySmokingField.childAt(0).instance().checked).toBe(false);
+  });
+
+  it('Should auto-select the radio button when provided property information', () => {
+    const container = mount(propertyFormComponent);
+    const smokingRadioField = container.find('#smokingInput');
+    const smokingField = smokingRadioField.childAt(1);
+    const nonSmokingField = smokingRadioField.childAt(2);
+    const anySmokingField = smokingRadioField.childAt(3);
+    expect(smokingField.childAt(0).instance().checked).toBe(false);
+    expect(nonSmokingField.childAt(0).instance().checked).toBe(false);
+    expect(anySmokingField.childAt(0).instance().checked).toBe(true);
+  });
+});
+
